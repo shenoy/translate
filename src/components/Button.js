096e8fd5ba82abd4e111ed5f413d073675731c13
RAJESH <rajeshtshenoy@gmail.com>
2019-12-05 10:58:48 +0000

    consumer added

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,12 +5,17 @@ class Button extends React.Component {
 
   static contextType = LanguageContext;
 
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
   render() {
-
-
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-
-    return <button className='ui button primary' >{text}</button>;
+    return (
+      <button className='ui button primary' >
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
 
   }
 }
