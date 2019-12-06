bd3a617b0782fa370a5b9bfd1af9c5ad718b9384
RAJESH <rajeshtshenoy@gmail.com>
2019-12-06 06:32:17 +0000

    languagestore component added

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,26 @@
 import React from 'react';
 
-export default React.createContext('english');
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+
+  state = { language: 'english' }
+
+  onLanguageChange = language => {
+
+    this.setState({ language });
+  };
+
+  render() {
+
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange }}>
+
+        {this.props.children}
+
+      </ Context.Provider>
+
+    );
+  }
+
+}
